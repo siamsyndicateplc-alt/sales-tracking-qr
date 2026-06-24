f06d156e0b2a1b712ab9f2b59531e6546134d9e0
Tawan Banthao <tawan.b@sst.co.th>
2026-06-24 11:41:38 +0700

    fix: use Unicode property escapes in sanitizeString to preserve Thai characters

--- routes/survey.js
+++ routes/survey.js
@@ -6,7 +6,7 @@ const { randomUUID } = require('crypto');
 // Helper functions for sanitizing inputs
 const sanitizeString = (str) => {
     if (typeof str !== 'string') return '';
-    return str.replace(/<[^>]*>/g, '').replace(/[^\u0E00-\u0E7Fa-zA-Z0-9\s\-_./()]/g, '').trim().substring(0, 100);
+    return str.replace(/<[^>]*>/g, '').replace(/[^\p{L}\p{N}\p{M}\s\-_./()]/gu, '').trim().substring(0, 100);
 };
 
 // \u0E2A\u0E33\u0E2B\u0E23\u0E31\u0E1A field \u0E17\u0E35\u0E48\u0E23\u0E31\u0E1A\u0E02\u0E49\u0E2D\u0E04\u0E27\u0E32\u0E21\u0E22\u0E32\u0E27 \u0E40\u0E0A\u0E48\u0E19 \u0E04\u0E27\u0E32\u0E21\u0E04\u0E34\u0E14\u0E40\u0E2B\u0E47\u0E19 \u0E41\u0E25\u0E30\u0E15\u0E31\u0E27\u0E40\u0E25\u0E37\u0E2D\u0E01\u0E1B\u0E23\u0E31\u0E1A\u0E1B\u0E23\u0E38\u0E07
